8d359f23ef7cb873df9381ac8e4e2a5a11d093b8
Marlon Bucciarelli <marlon.bucciarelli@netlight.com>
2025-09-30 20:18:53 +0200

    Create inital repo

---
+++ src/main/kotlin/com/app/datefinderservice/DatefinderserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.app.datefinderservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DatefinderserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<DatefinderserviceApplication>(*args)
+}
---
+++ src/test/kotlin/com/app/datefinderservice/DatefinderserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.app.datefinderservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DatefinderserviceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
